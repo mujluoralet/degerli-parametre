eab209e52b641c26e0fb6ddb9d4cf3a4a5f154e5
kadir30 <kadir30@BPC01>
2023-12-05 09:20:24 +0300

    Proje dosyası ekle.

---
+++ parametre/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parametre
+{
+    class Sinifim
+    {
+        public void Selamla(string ad = "roronoa zoro")
+        {
+            Console.WriteLine("Kon'nichiwa " + ad);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinifim s = new Sinifim();
+            s.Selamla();
+            s.Selamla("monkey D. luffy");
+        }
+    }
+}
